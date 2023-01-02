d12e7f5eb7a15553026febc286f17a1336ab78dd
Nassima <54705839+NassimaKheldouni@users.noreply.github.com>
2023-01-02 11:20:00 +0100

    Update streamlit_app.py
    added header

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,7 @@ st.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response 
 import requests
+st.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response)
