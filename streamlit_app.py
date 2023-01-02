6ebd911e97f03c7c7fa5d6902e21a9c133f408db
Nassima <54705839+NassimaKheldouni@users.noreply.github.com>
2023-01-02 11:28:14 +0100

    Update streamlit_app.py
    adding textbox for fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,10 @@ st.dataframe(fruits_to_show)
 #New section to display fruityvice api response 
 import requests
 st.header("Fruityvice Fruit Advice!")
+
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # Normalize the fruityvice json response
